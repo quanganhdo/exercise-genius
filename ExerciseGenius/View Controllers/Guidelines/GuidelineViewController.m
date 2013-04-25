d03c0ea05a0447c01a8df09e3dc043333a342ea2
Anh Quang Do <quanganhdo@gmail.com>
2013-04-25 14:17:50 -0500

    Guidelines mockup

---
+++ ExerciseGenius/View Controllers/Guidelines/GuidelineViewController.m
@@ -0,0 +1,21 @@
+//
+//  Created by Anh Quang Do on 4/25/13.
+//  Copyright Anh Quang Do. All rights reserved.
+//
+
+
+#import "GuidelineViewController.h"
+#import "SDSegmentedControl.h"
+
+@interface GuidelineViewController ()
+
+@property (nonatomic) IBOutlet SDSegmentedControl *segmentedControl;
+@property (nonatomic) IBOutlet UITextView         *textView;
+
+@end
+
+@implementation GuidelineViewController {
+
+}
+
+@end
